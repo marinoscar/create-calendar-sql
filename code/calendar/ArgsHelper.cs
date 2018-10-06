f0a5c640b47699b75a9aaa8df6ec3ee4d0b164f6
Oscar Marin <oscar@marin.cr>
2018-10-06 06:12:16 -0600

    added the functionality to generate a file

---
+++ code/calendar/ArgsHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calendar
+{
+    public class ArgsHelper
+    {
+        public ArgsHelper(IEnumerable<string> args)
+        {
+            Args = new List<string>(args);
+        }
+
+        protected List<string> Args { get; private set; }
+        public static string ShowHelp()
+        {
+            return @"
+Use the following commands
+/start      {num}  Year to start for example 2018
+/years      {num}  Count of years to increase, for example 2
+/format     {text} csv or sql
+/output     {text} the location of the file to store the values
+";
+        }
+
+        public string GetArg(string sw)
+        {
+            var idx = Args.IndexOf(sw);
+            if (idx < 0) throw new ArgumentException("Invalid switch");
+            var valIdx = idx + 1;
+            if (valIdx >= Args.Count) throw new ArgumentException("Value is missing");
+            var val = Args[valIdx];
+            if (val.Trim().StartsWith("/")) throw new ArgumentException("Value is missing");
+            return val;
+        }
+
+
+        public short Start
+        {
+            get { return ToNum(GetArg("/start")); }
+        }
+
+        public short Years
+        {
+            get { return ToNum(GetArg("/years")); }
+        }
+
+        public string Format
+        {
+            get { return GetArg("/format").Trim().ToLowerInvariant(); }
+        }
+
+        public string Output
+        {
+            get { return GetArg("/output").Trim(); }
+        }
+
+        private short ToNum(string val)
+        {
+            if (!short.TryParse(val, out short res)) throw new ArgumentException("Invalid value");
+            return res;
+        }
+
+
+    }
+}
--- code/calendar/Program.cs
+++ code/calendar/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,35 @@ namespace calendar
     {
         static void Main(string[] args)
         {
+            var options = new ArgsHelper(args);
+            if (args.Length <= 0)
+            {
+                Console.WriteLine(ArgsHelper.ShowHelp());
+                return;
+            }
+            try
+            {
+                CheckFormat(options.Format);
+                if (options.Format == "csv") SaveToFile(() => DateHelper.GetCsv(options.Start, options.Years), options.Output);
+                else SaveToFile(() => DateHelper.GetSqlScript(options.Start, options.Years), options.Output);
+            }
+            catch (ArgumentException argEx)
+            {
+                Console.WriteLine(argEx.Message);
+                Console.WriteLine();
+                Console.WriteLine(ArgsHelper.ShowHelp());
+            }
+        }
+        static void CheckFormat(string format)
+        {
+            if (format != "csv" && format != "sql") throw new ArgumentException("/format value needs to be either csv or sql");
+        }
+
+        static void SaveToFile(Func<string> generate, string file)
+        {
+            File.WriteAllText(file, generate());
         }
     }
+
+
 }
